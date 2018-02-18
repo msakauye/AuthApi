ad60406a34e2ad3b2aa76d4c160424360e7d7ee8
Mark Sakauye <msakauye@uthsc.edu>
2018-02-18 01:57:05 -0500

    cors

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB setup
 mongoose.connect('mongodb://localhost/authapi');
 
 // App Setup
+app.use(cors()); // allow calls to api from everywhere
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
--- router.js
+++ router.js
@@ -11,7 +11,7 @@ const requireSignin = passport.authenticate('local', { session: false });
 module.exports = function(app) {
     // example on how to use requireAuth:
     app.get('/', requireAuth, function(req, res) {
-        res.send({ success: true });
+        res.send({ message: 'Super secret code is ABC123' });
     })
     app.post('/signin', requireSignin, Authentication.signin);
     app.post('/signup', Authentication.signup);
