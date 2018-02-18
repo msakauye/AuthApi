b1fe3f77cb67645fc64f01d81fd9d45540b1f979
Mark Sakauye <msakauye@uthsc.edu>
2018-02-18 00:17:15 -0500

    signin route

--- router.js
+++ router.js
@@ -1,5 +1,18 @@
 const Authentication = require('./controllers/Authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+// used to verify a valid jwt token in a request
+const requireAuth = passport.authenticate('jwt', { session: false });
+
+// used to verify a valid email/password credentials
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
+    // example on how to use requireAuth:
+    app.get('/', requireAuth, function(req, res) {
+        res.send({ success: true });
+    })
+    app.post('/signin', requireSignin, Authentication.signin);
     app.post('/signup', Authentication.signup);
 }
--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,34 @@ const User = require('../models/User');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// Create local strategy
+// Tell where to look in our request body for our username and password fields
+// by default assumes 'username' and 'password'
+const localOptions = {
+    usernameField: 'email' // look at 'email' field for our username
+    // passwordField: 'pass' // can also define another field for our password
+}
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+    // verify this username and password
+    // if valid credentials, call done with the user
+    // else, call done with false
+    User.findOne({ email: email }, function(err, user) {
+        if (err) { return done(err); }
+
+        if (!user) { return done(null, false); }
+
+        // compare passwords - is `password` === user.password?
+        // comparePassword is a function we defined on the User model in /models/User.js
+        user.comparePassword(password, function(err, isMatch) {
+            if (err) { return done(err); }
+            if (!isMatch) { return done(null, false); }
+
+            return done(null, user);
+        });
+    });
+});
 
 // Setup options for JWT Strategy
 const jwtOptions = {
@@ -32,3 +60,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 
 // Tell passport to use this strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
